f8ec9f724ed10cd9609216c571b6724180c8af3f
FrancisUzoigwe <kossyuzoigwe@gmail.com>
2023-10-06 16:42:21 +0100

    pull first

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppin: "Poppin"
+      },
+      screen: {
+        max: {
+          main: "320px"
+        },
+        max: {
+          medium: "690px"
+        }
+      }
+    },
   },
   plugins: [],
-}
-
+};
